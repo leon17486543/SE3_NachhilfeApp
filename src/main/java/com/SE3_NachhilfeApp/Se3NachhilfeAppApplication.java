605018b40913023d2542a5f487f0828ff6af6d2f
leon17486543 <95215676+leon17486543@users.noreply.github.com>
2022-12-05 11:58:21 +0100

    create project

---
+++ src/main/java/com/SE3_NachhilfeApp/Se3NachhilfeAppApplication.java
@@ -0,0 +1,13 @@
+package com.SE3_NachhilfeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se3NachhilfeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Se3NachhilfeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SE3_NachhilfeApp/Se3NachhilfeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SE3_NachhilfeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se3NachhilfeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
